3a2631a25049de03eb4119515669a90a0858a3b5
IMMPROVED-BS <159400406+IMMPROVED-BS@users.noreply.github.com>
2024-07-22 14:19:54 +0530

    upd

--- android/app/src/main/java/com/aetherdenn/MainActivity.kt
+++ android/app/src/main/java/com/aetherdenn/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bucket
+package com.aetherdenn
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "bucket"
+  override fun getMainComponentName(): String = "aetherdenn"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/aetherdennTests/aetherdennTests.m
+++ ios/aetherdennTests/aetherdennTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bucketTests : XCTestCase
+@interface aetherdennTests : XCTestCase
 
 @end
 
-@implementation bucketTests
+@implementation aetherdennTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
